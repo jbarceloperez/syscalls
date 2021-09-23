85e1ddf2d15413403b769410c5b5e60455998c4d
jbarceloperez <javibarpe@hotmail.com>
2021-09-23 12:46:49 +0200

    ª

---
+++ Semana2/openimg.c
@@ -0,0 +1,15 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return EXIT_SUCCESS;
+}
